6549b562126b7c6dcddb2b396ea28d4c17bffe17
a21step3 <a21step3@gmail.com>
2024-06-30 16:07:41 -0400

    fixed include path

--- app/src/caps_word.c
+++ app/src/caps_word.c
@@ -1,6 +1,6 @@
 #include <zmk/caps_word.h>
 
-#include <stdbool.h>
-#include <kernel.h>
+#include <zephyr/stdbool.h>
+#include <zephyr/kernel.h>
 
 bool zmk_caps_word_state() { return last_state_of_caps_word; }
