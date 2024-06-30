11b4c2e0d23a720ef07b19cc4d7787b3ac21388d
a21step3 <a21step3@gmail.com>
2024-06-30 17:33:39 -0400

    swapped enpoint for transport

--- app/src/rgb_underglow.c
+++ app/src/rgb_underglow.c
@@ -225,7 +225,7 @@ static void zmk_rgb_underglow_effect_status(void) {
 // ------- Turn on the output status led -------
 #if IS_ENABLED(CONFIG_ZMK_RGB_UNDERGLOW_STATUS_OUTPUT)
     // status_hsb.h = zmk_endpoints_selected() * 90;
-    status_hsb.h = hue_scale_to_range(zmk_endpoint_instance(), ZMK_TRANSPORT_BLE,
+    status_hsb.h = hue_scale_to_range(zmk_transport(), ZMK_TRANSPORT_BLE,
                                       CONFIG_ZMK_RGB_UNDERGLOW_STATUS_OUTPUT_COLOR_MIN,
                                       CONFIG_ZMK_RGB_UNDERGLOW_STATUS_OUTPUT_COLOR_MAX);
     pixels[CONFIG_ZMK_RGB_UNDERGLOW_STATUS_OUTPUT_N] = hsb_to_rgb(hsb_scale_min_max(status_hsb));
