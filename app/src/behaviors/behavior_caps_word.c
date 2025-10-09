c2b9329a29a45aca27471d636129fbe1c25579a6
a21step3 <a21step3@gmail.com>
2025-10-09 15:19:25 -0400

    testing different options for last_state

--- app/src/behaviors/behavior_caps_word.c
+++ app/src/behaviors/behavior_caps_word.c
@@ -21,8 +21,6 @@
 #include <zmk/keymap.h>
 #include <zmk/caps_word.h>
 
-bool last_state_of_caps_word = false;
-
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
@@ -41,6 +39,7 @@ struct behavior_caps_word_config {
 
 struct behavior_caps_word_data {
     bool active;
+    bool last_state_of_caps_word;
 };
 
 static void activate_caps_word(const struct device *dev) {
