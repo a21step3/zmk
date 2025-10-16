2f5639611f7c087e70ca3467bb52068700184970
a21step3 <a21step3@gmail.com>
2025-10-16 18:30:52 -0400

    added state to .binding_pressed

--- app/src/behaviors/behavior_caps_word.c
+++ app/src/behaviors/behavior_caps_word.c
@@ -78,14 +78,14 @@ bool zmk_caps_word_state(struct zmk_behavior_binding *binding,
     struct behavior_caps_word_data *data = dev->data;
 
     if (data->active) {
-        return 1;
-    } else {
         return 0;
+    } else {
+        return 1;
     }
-};
+}
 
 static const struct behavior_driver_api behavior_caps_word_driver_api = {
-    .binding_pressed = on_caps_word_binding_pressed,
+    .binding_pressed = on_caps_word_binding_pressed,zmk_caps_word_state,
     .binding_released = on_caps_word_binding_released,
 #if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
     .get_parameter_metadata = zmk_behavior_get_empty_param_metadata,
