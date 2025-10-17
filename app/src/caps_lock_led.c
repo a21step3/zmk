6c2c27c0a9b17fc0659413abe0547e341e13f298
a21step3 <a21step3@gmail.com>
2025-10-17 11:27:47 -0400

    changed caps led lighting code

--- app/src/caps_lock_led.c
+++ app/src/caps_lock_led.c
@@ -11,10 +11,10 @@
 
 static void set_capslock_led(bool caps_on) {
     struct zmk_led_hsb caps_word_hsb = caps_on ? 
-	   (struct zmk_led_hsb caps_word_hsb = state.color;
+	   (state.color;
             caps_word_hsb.h = 1 * CONFIG_ZMK_RGB_UNDERGLOW_STATUS_CAPS_COLOR;
             caps_word_hsb.b = 1 * caps_word_hsb.b;) :
-	   (struct zmk_led_hsb caps_word_hsb = state.color;
+	   (state.color;
             caps_word_hsb.h = 0 * CONFIG_ZMK_RGB_UNDERGLOW_STATUS_CAPS_COLOR;
             caps_word_hsb.b = 0 * caps_word_hsb.b;);
 }
@@ -36,3 +36,21 @@ static int capslock_init(const struct device *dev) {
 }
 
 SYS_INIT(capslock_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
+// C program to check whether a year is leap year or not
+// using ternary operator
+
+#include <stdio.h>
+
+int main()
+{
+    int yr = 1900;
+
+    (yr%4==0) ? (yr%100!=0? printf("The year %d is a leap year",yr)
+     : (yr%400==0 ? printf("The year %d is a leap year",yr)
+         : printf("The year %d is not a leap year",yr)))
+             : printf("The year %d is not a leap year",yr);
+    return 0;
+}
+
+//This code is contributed by Susobhan AKhuli
