8bb16f0d452396f6665f3411667e868542d84c32
a21step3 <a21step3@gmail.com>
2025-10-19 12:34:53 -0400

    using zmk_endpoint_selection_changed for OUTPUT status

--- app/src/rgb_underglow.c
+++ app/src/rgb_underglow.c
@@ -31,7 +31,7 @@
 #include <zmk/endpoints.h>
 #include <zmk/ble.h>
 #include <zmk/caps_word.h>
-#include <zmk/events/endpoint_selection_changed.h>
+#include <zmk/hid_indicators.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
@@ -226,7 +226,7 @@ static void zmk_rgb_underglow_effect_status(void) {
 // ------- Turn on the output status led -------
 #if IS_ENABLED(CONFIG_ZMK_RGB_UNDERGLOW_STATUS_OUTPUT)
     //status_hsb.h = zmk_endpoints_selected() * 90;
-    status_hsb.h = hue_scale_to_range(zmk_endoint_selection_changed, ZMK_TRANSPORT_BLE,
+    status_hsb.h = hue_scale_to_range(zmk_hid_indicators_get_profile(), ZMK_TRANSPORT_BLE,
       				      CONFIG_ZMK_RGB_UNDERGLOW_STATUS_OUTPUT_COLOR_MIN,
         			      CONFIG_ZMK_RGB_UNDERGLOW_STATUS_OUTPUT_COLOR_MAX);
             pixels[CONFIG_ZMK_RGB_UNDERGLOW_STATUS_OUTPUT_N] = hsb_to_rgb(hsb_scale_min_max(status_hsb));
