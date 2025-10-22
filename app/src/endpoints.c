61c1fbc2ac499881cf7bf5d5c0270f3c23b6af56
a21step3 <a21step3@gmail.com>
2025-10-22 15:06:45 -0400

    added output_selected enum

--- app/src/endpoints.c
+++ app/src/endpoints.c
@@ -33,9 +33,9 @@ static enum zmk_transport preferred_transport =
 static void update_current_endpoint(void);
 
 enum zmk_output zmk_output_selected() {
-    struct zmk_endpoint_instance current_instance = output;
+    struct zmk_endpoint_instance current_instance;
 
-    return output;
+    return current_instance;
 }
 
 #if IS_ENABLED(CONFIG_SETTINGS)
